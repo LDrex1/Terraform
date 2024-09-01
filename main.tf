a10a65ba14c01b1da6c5ae0f35230df1e6b1025d
Ldrex1 <damzealwini7@gmail.com>
2024-09-01 15:10:33 +0100

    update: module block for compute instances

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
 provider "aws" {
-  region     = "eu-west-2"
+  region = "eu-west-2"
 }
 
-resource "aws_instance" "linux_server" {
-    ami = "ami-01ec84b284795cbc7"
-    instance_type = "t2.micro"
-
-    tags ={
-        Name:"Cloud_infra_ter"
-    }
+module "ec2_instances"{
+    source ="./modules/compute"
 }
+
 
